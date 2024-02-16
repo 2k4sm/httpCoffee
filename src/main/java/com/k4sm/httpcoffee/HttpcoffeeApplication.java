f90f70b333264924391be94481ced46835778d61
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-02-16 13:18:52 +0530

    adds SpringBoot init files and deps.

---
+++ src/main/java/com/k4sm/httpcoffee/HttpcoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.k4sm.httpcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpcoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpcoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k4sm/httpcoffee/HttpcoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k4sm.httpcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpcoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
